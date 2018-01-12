472e61bb547fadc2def9db74d421c647a42f5841
Unknown <10837515@my.uvu.edu>
2018-01-12 09:53:41 -0700

    Added beginning of game
    Start sayings
    and first guess

--- NumberGuesser/Assets/Scripts Folder/GuesserScript.cs
+++ NumberGuesser/Assets/Scripts Folder/GuesserScript.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class GuesserScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        print("Our game has started!");
-	}
+    public bool winCondition = false;
+    public int max;
+    public int min;
+    public int guess;
+
+    // Use this for initialization
+    private void Start() {
+
+        print("Welcome to... \n\tNumber Guesser!");
+        print("Pick a number between " + min + " and " + max + "!");
+
+        //Is the value Guess
+        print("Is the value" + guess + "?");
+
+        //instructions - push these buttons
+        print("Press the up arrow if your number is higher \n the down arrow if your number is lower, \n and enter if I'm correct.");
+        
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	public void Update() {
+        if (winCondition == true)
+        {
+            print("I win! Thanks for playing!");
+        }
 	}
 }
