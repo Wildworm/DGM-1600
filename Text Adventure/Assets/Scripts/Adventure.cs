2af3cb11714155ab8d8db79b9a4fb24d6034a26c
Unknown <10837515@my.uvu.edu>
2018-01-22 09:40:17 -0700

    continued work on ball roller, Started Text Adventure
    Added walls to ball roller
    Created Text adventure game project

---
+++ Text Adventure/Assets/Scripts/Adventure.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adventure : MonoBehaviour {
+
+    public enum States { Bedroom, Window, Door, Bed };
+    public States places;
+
+    // Use this for initialization
+    void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
