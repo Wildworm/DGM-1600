6b12a9484a65cd7beb0cbe1373b0b09fc63f059f
Unknown <10837515@my.uvu.edu>
2018-02-02 09:54:41 -0700

    Started Brick Breaker game

--- NumberGuesser/Assets/Scripts Folder/GuesserScript.cs
+++ NumberGuesser/Assets/Scripts Folder/GuesserScript.cs
@@ -8,40 +8,40 @@ public class GuesserScript : MonoBehaviour {
     public int max;
     public int min;
     int guess;
-    int count = 1;
+    int count;
+   
 
     // Use this for initialization
     private void Start() {
+        max = max + 1;
 
         print("Welcome to... \n\tNumber Guesser!");
         print("Pick a number between " + min + " and " + max + "!");
 
         //Is the value Guess
         Guess(count);
-        count = count++;
+        count = count--;
 
         //instructions - push these buttons
         print("Press the up arrow if your number is higher \n the down arrow if your number is lower, \n and enter if I'm correct.");
 
-        max = max + 1;
+        
     }
 
     private void Guess(int count)
     {
-        if (count == 1)
-            guess = Random.Range(400, 600);
-        else
-            guess = Random.Range(min, max);
-
+        guess = Random.Range(min, max);
+        
         print("Is the number " + guess + "?");
         print(count);
+
     }
     //count variable broken**************************
 	
 	// Update is called once per frame
 	public void Update() {
 
-        if (count == maxGuesses)
+        if (count == 0)
         {
             print("Well... \nyou win...");
             Application.Quit();
@@ -56,7 +56,7 @@ public class GuesserScript : MonoBehaviour {
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             min = guess + 1;
-            count = count++;
+            count = count--;
             Guess(count);
            
         }
@@ -65,7 +65,7 @@ public class GuesserScript : MonoBehaviour {
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             max = guess - 1;
-            count = count++;
+            count = count--;
             Guess(count);
            
         }
