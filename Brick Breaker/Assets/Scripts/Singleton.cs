bdcb95e9a4b50e5e054e22f39eb67fdb2641cf21
Unknown <10837515@my.uvu.edu>
2018-02-09 10:00:12 -0700

    Updated Brick Breaker
    Added Singleton to managers
    added game over scenes (needs work)
    updated player movement (still broken)
    added Boundaries
    Created multiple brick prefabs
    add some extra organization

---
+++ Brick Breaker/Assets/Scripts/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+
+    // Use this for initialization
+    public static Singleton instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+        {
+            //if not, set instance to this
+            instance = this;
+        }
+        //If instance already exists and it's not this:
+        else if (instance != this)
+        {
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+        }
+
+    }
+}
